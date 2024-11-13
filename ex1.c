9a8a56aa2c595190d46453628c739128c3174e49
orizaltsman <orizaltsman2008@gmail.com>
2024-11-14 00:40:04 +0200

    did 1 ,4

--- ex1.c
+++ ex1.c
@@ -11,11 +11,23 @@ int main() {
   
   // What bit
   printf("What bit:\n");
+  int bit, num;
+  printf("Please enter a number:\n");
+  scanf(" %d", &num);
+  printf("Please enter a position:\n");
+  scanf(" %d", &bit);
+  printf("%d\n",1&(num>>bit));
   /*Scan two integers (representing number and a position)
   Print the bit in this position. */
   
   // Set bit
   printf("\nSet bit:\n");
+  printf("Please enter a number:\n");
+  scanf(" %d", &num);
+  printf("Please enter a position:\n");
+  scanf(" %d", &bit);
+  printf("%d\n",num|(bit<<1));
+  printf("%d\n",num&~(bit<<1));
   /*Scan two integers (representing number and a position)
   Make sure the bit in this position is "on" (equal to 1)
   Print the output
@@ -27,11 +39,20 @@ int main() {
   /*Scan two integers (representing number and a position)
   Toggle the bit in this position
   Print the new number */
+  printf("Please enter a number:\n");
+  scanf(" %d", &num);
+  printf("Please enter a position:\n");
+  scanf(" %d", &bit);
+  printf("%d\n",bit);
+
   
   // Even - Odd
   printf("\nEven - Odd:\n");
   /* Scan an integer
   If the number is even - print 1, else - print 0. */
+  printf("Please enter a number:\n");
+  scanf(" %d", &num);
+  printf("%d\n",(num&1)^1);
   
   // 3, 5, 7, 11
   printf("\n3, 5, 7, 11:\n");
